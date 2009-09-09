f61186ab80cbe1d376377e4bac827bec024f5f27
Vitaliy Filippov <vitalif@yourcmc.ru>
2009-09-09 14:06:27 +0000

    Bug 54081 FlvHandler extension

---
+++ FlvHandler.i18n.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Internationalisation file for FlvHandler extension.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'flvhandler_desc'   => 'Allow Flash Video (.flv) files to be used in standard image tags (e.g. <nowiki>[[Image:Movie.flv]]</nowiki>)',
+	'flv-long-desc'     => '(Flash video, $1 × $2 pixels, file size: $3)'
+);
+
+?>
